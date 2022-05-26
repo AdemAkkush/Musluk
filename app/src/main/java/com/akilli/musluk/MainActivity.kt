5ce07535fa670d9687e5839eb19dc1711823f6af
AdemAkkush <akkus.adem@hotmail.com>
2022-05-26 21:10:26 +0300

    update

--- app/src/main/java/com/akilli/musluk/MainActivity.kt
+++ app/src/main/java/com/akilli/musluk/MainActivity.kt
@@ -12,62 +12,113 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.akilli.musluk.ui.theme.AkilliMuslukTheme
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
-            var state by remember {
-                mutableStateOf(WaterData())
+            var flow by remember {
+                mutableStateOf(0.0)
             }
+
+            var waterTemperature by remember {
+                mutableStateOf(0.0)
+            }
+
+            var totalWaterUsage by remember {
+                mutableStateOf(0.0)
+            }
+
+            var unitPrice by remember {
+                mutableStateOf(0.0)
+            }
+
+            var openCount by remember {
+                mutableStateOf(0)
+            }
+
+            var price by remember {
+                mutableStateOf(0.0)
+            }
+            DisposableEffect(key1 = true, effect = {
+                val flowListener = addFloatListener("flow") {
+                    flow = it
+                }
+                val temperatureListener = addFloatListener("temperature") {
+                    waterTemperature = it
+                }
+                val usageListener = addFloatListener("totalUsage") {
+                    totalWaterUsage = it
+                }
+                val unitPriceListener = addFloatListener("unitPrice") {
+                    unitPrice = it
+                }
+                val openCountListener = addFloatListener("openCount") {
+                    openCount = it.toInt()
+                }
+                onDispose {
+                    Firebase.database.reference.removeEventListener(flowListener)
+                    Firebase.database.reference.removeEventListener(temperatureListener)
+                    Firebase.database.reference.removeEventListener(usageListener)
+                    Firebase.database.reference.removeEventListener(unitPriceListener)
+                    Firebase.database.reference.removeEventListener(openCountListener)
+                }
+            })
             AkilliMuslukTheme {
-                DisposableEffect(key1 = true, effect = {
-                    val listener = Firebase.firestore
-                        .collection("veri")
-                        .document("user1")
-                        .addSnapshotListener { value, error ->
-                            if (error != null) {
-                                state = WaterData()
-                            } else if (value != null && value.exists()) {
-                                state = value.toObject(WaterData::class.java) ?: WaterData()
-                            }
-                        }
-                    onDispose {
-                        listener.remove()
-                    }
-                })
 
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.SpaceEvenly,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween){
-                            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(start = 12.dp)) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(start = 12.dp)
+                            ) {
                                 Text(text = "Su Akış Miktarı")
-                                Text(text = "${state.waterFlow} L/M", fontWeight = FontWeight.SemiBold)
+                                Text(text = "$flow L/M", fontWeight = FontWeight.SemiBold)
                             }
-                            Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.padding(end = 12.dp)) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(end = 12.dp)
+                            ) {
                                 Text(text = "Su Sıcaklığı")
-                                Text(text = "${state.waterTemperature} C", fontWeight = FontWeight.SemiBold)
+                                Text(text = "$waterTemperature C", fontWeight = FontWeight.SemiBold)
                             }
                         }
-                        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(top = 36.dp)) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(top = 36.dp)
+                        ) {
                             Text(text = "Su Açılma Sayısı")
-                            Text(text = "${state.openedCount}" ,fontWeight = FontWeight.SemiBold)
+                            Text(text = "$openCount", fontWeight = FontWeight.SemiBold)
                         }
                     }
 
-                    Column (horizontalAlignment = Alignment.CenterHorizontally){
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
                         Column(horizontalAlignment = Alignment.CenterHorizontally) {
                             Text(text = "Su Fiyatı")
-                            Text(text = "${state.price} TL", fontWeight = FontWeight.SemiBold)
+                            Text(text = "$price TL", fontWeight = FontWeight.SemiBold)
                         }
                         Button(onClick = {
-                            state=state.copy(price = state.unitPrice*state.totalWaterUsage)
+                            price = unitPrice * totalWaterUsage
                         }) {
                             Text(text = "Fatura Hesapla")
                         }
@@ -77,13 +128,25 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+    private fun addFloatListener(value: String, onChange: (Double) -> Unit): ValueEventListener {
+        return Firebase.database("https://bitirme-projesi-f45f8-default-rtdb.europe-west1.firebasedatabase.app")
+            .reference
+            .child(value).addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    println(snapshot)
+                    onChange((snapshot.value as Number).toDouble())
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
+    }
 }
 
-data class WaterData(
-    val openedCount: Int = 0,
-    val unitPrice: Float = 0f,
-    val waterFlow: Float = 0f,
-    val waterTemperature: Float = 0f,
-    val price:Float=0f,
-    val totalWaterUsage:Float=0f
-)
